3212cc8dd83e218c338cc4abc4777e7e659edb0c
Russ Green <1886088+russgreen@users.noreply.github.com>
2021-11-13 20:15:57 +0000

    initial commit

---
+++ RevitAddinTemplate/App.cs
@@ -0,0 +1,105 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+//using Microsoft.AppCenter;
+//using Microsoft.AppCenter.Analytics;
+//using Microsoft.AppCenter.Crashes;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace RevitAddinTemplate
+{
+    public class App : IExternalApplication
+    {
+        // get the absolute path of this assembly
+        public static readonly string ExecutingAssemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+        public static UIControlledApplication CachedUiCtrApp;
+        public static UIApplication CachedUiApp;
+
+        public static Autodesk.Revit.DB.Document RevitDocument;
+
+        private readonly string _tabName = "Revit Addin Template";
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //start the application center monitoring - setup app on app center and uncomment thos code
+            //AppCenter.LogLevel = LogLevel.Verbose;
+            //System.Windows.Forms.Application.ThreadException += (sender, args) =>
+            //{
+            //    Crashes.TrackError(args.Exception);
+            //};
+
+            //AppCenter.Start("<APPGUID>", typeof(Crashes));
+
+            CachedUiCtrApp = application;
+
+            var panel = RibbonPanel(application);
+
+            //application.Idling += new EventHandler<IdlingEventArgs>(OnIdling);
+            //application.ViewActivated += new EventHandler<ViewActivatedEventArgs>(OnViewActivated);
+            //application.ApplicationClosing += new EventHandler<ApplicationClosingEventArgs>(ApplicationClosing);
+
+            return Result.Succeeded;
+        }
+
+
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+
+        #region Event Handling
+        private void OnIdling(object sender, IdlingEventArgs e)
+        {
+        }
+
+        private void OnViewActivated(object sender, ViewActivatedEventArgs e)
+        {
+        }
+
+        private void ApplicationClosing(object sender, ApplicationClosingEventArgs e)
+        {
+        }
+        #endregion
+
+        #region Ribbon Panel
+
+        private RibbonPanel RibbonPanel(UIControlledApplication application)
+        {
+            try
+            {
+                CachedUiCtrApp.CreateRibbonTab(_tabName);
+            }
+            catch { }
+
+            RibbonPanel panel = CachedUiCtrApp.CreateRibbonPanel(_tabName, "RevitAddin_Panel");
+            panel.Title = "Revit addin";
+
+            PushButtonData pbData = new PushButtonData("Command", "Command", Assembly.GetExecutingAssembly().Location, "RevitAddinTemplate.Command");
+            PushButton pb = (PushButton)panel.AddItem(pbData);
+            pb.ToolTip = "Tooltip text";
+            pb.LargeImage = PngImageSource("RevitAddinTemplate.Resources.code-small.png");
+
+            return panel;
+        }
+
+        private System.Windows.Media.ImageSource PngImageSource(string embeddedPath)
+        {
+            var stream = GetType().Assembly.GetManifestResourceStream(embeddedPath);
+            System.Windows.Media.ImageSource imageSource;
+            try
+            {
+                imageSource = BitmapFrame.Create(stream);
+            }
+            catch
+            {
+                imageSource = null;
+            }
+
+            return imageSource;
+        }
+        #endregion
+    }
+}
---
+++ RevitAddinTemplate/Command.cs
@@ -0,0 +1,59 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Diagnostics;
+
+namespace RevitAddinTemplate
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+#if REVIT2021
+#else
+#endif
+
+#if PREFORGETYPEID
+#else
+#endif
+            TaskDialog.Show("Revit addin", "Testing the command is being called");
+
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // Access current selection
+
+            Selection sel = uidoc.Selection;
+
+            // Retrieve elements from database
+
+            FilteredElementCollector col
+              = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfCategory(BuiltInCategory.INVALID)
+                .OfClass(typeof(Wall));
+
+            // Filtered element collector is iterable
+
+            foreach (Element e in col)
+            {
+                Debug.Print(e.Name);
+            }
+
+            // Modify document within a transaction
+
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Transaction Name");
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
